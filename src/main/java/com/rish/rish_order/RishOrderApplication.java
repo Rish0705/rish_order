d7bb8dba4eefc21c8a99c6cb5d71c618a5064282
金文辉 <rish0705@163.com>
2023-03-06 17:52:04 +0800

    first commit

---
+++ src/main/java/com/rish/rish_order/RishOrderApplication.java
@@ -0,0 +1,13 @@
+package com.rish.rish_order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RishOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RishOrderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rish/rish_order/RishOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rish.rish_order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RishOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
